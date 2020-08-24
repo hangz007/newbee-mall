dff709693c124164cc239b8a867ab2553896e1f9
hangz007 <hangz00749@163.com>
2020-08-24 10:41:37 +0800

    项目初始化以及测试

---
+++ src/main/java/ltd/newbee/mall/dao/UserDao.java
@@ -0,0 +1,40 @@
+package ltd.newbee.mall.dao;
+import ltd.newbee.mall.entity.User;
+import java.util.List;
+
+/**
+ * @author 13
+ * MyBatis 测试
+ */
+public interface UserDao {
+    /**
+     * 返回数据列表
+     *
+     * @return
+     */
+    List<User> findAllUsers();
+
+    /**
+     * 添加
+     *
+     * @param User
+     * @return
+     */
+    int insertUser(User User);
+
+    /**
+     * 修改
+     *
+     * @param User
+     * @return
+     */
+    int updUser(User User);
+
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
+    int delUser(Integer id);
+}
